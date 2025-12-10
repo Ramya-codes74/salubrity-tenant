4602503965503614d10c59fa038aa4117f468567
Ramya-codes74 <ramyapramyap416@gmail.com>
2025-12-10 12:29:21 +0530

    build success

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/salubrity-trnant/",
   plugins: [react()],
   server: {
     port: 5174, // Change to your desired port
